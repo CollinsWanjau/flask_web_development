74e6f2afb11e660f3d52e7fc7d58eb215d75dcdd
colloso999 <tripperskripper@gmail.com>
2023-07-15 03:12:04 +0300

    version one of my app

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 # importing flask class
 from flask import Flask
 
+
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!<h1>'
 
 @app.route("/")
 def hello_world():
