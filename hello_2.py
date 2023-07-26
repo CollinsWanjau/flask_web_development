4b783bf4e3ffe5710f5737c2ea08e14b5fd08df7
colloso999 <tripperskripper@gmail.com>
2023-07-26 11:29:12 +0300

    Send Asynchronous Email

--- hello_2.py
+++ hello_2.py
@@ -9,6 +9,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail, Message
+from threading import Thread
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -104,13 +105,25 @@ class User(db.Model):
 def make_shell_context():
     return {'app': app, 'db': db, 'User': User, 'Role': Role}
 
+# Function to send email asynchronously
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 # Email support
 def send_email(to, subject, template, **kwargs):
     msg = Message(app.config['IMAGINESTUDIOS_MAIL_SUBJECT_PREFIX'] + subject,
                   sender=app.config['IMAGINESTUDIOS_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    mail.send(msg)
+
+    # Create a new thread to send the email asynchronously
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+
+    # Return the thread object to allow monitoring the email sending status
+    return thr
+    # mail.send(msg)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
