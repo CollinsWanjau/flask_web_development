465cb4ba1c6b15207d84de1b4cceb1898f9a9ac7
colloso999 <tripperskripper@gmail.com>
2023-07-29 23:54:59 +0300

    User registration form

--- flasky/app/auth/forms.py
+++ flasky/app/auth/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
-
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
@@ -9,3 +10,33 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                              'Usernames must have only letters, '
+                                              'number, dotes or underscores')])
+    password = PasswordField('Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_email(self, field):
+        """
+        You can define custom validation methods by prefixing the field name
+        with "validate_" folowed by the name of the field you want to validate
+
+        User.query... : Queries the db to check if there is any user with
+        the provided email
+        """
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        """
+        Validating a username
+        """
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Username already in use.')
