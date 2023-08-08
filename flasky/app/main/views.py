0f1270f5d8789b2d9ae39982595243b2179abdc5
colloso999 <tripperskripper@gmail.com>
2023-08-08 12:34:09 +0300

    Profile edit route

--- flasky/app/main/views.py
+++ flasky/app/main/views.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from flask import render_template, session, redirect, url_for, current_app
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm
 from .. import db
 from ..models import User
 from flask import flash, Blueprint, send_from_directory
 from flask_login import login_required, current_user
 import os
 from ..email import send_email
-# from decorators import admin_required, permission_required
+from ..decorators import admin_required, permission_required
 
 # main = Blueprint('main', __name__)
 
@@ -95,3 +95,53 @@ def for_admins_only():
 def for_moderators_only():
     return "For comment moderators!"
 """
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    """
+    The view function sets initial values for all the fields before presenting
+    the form.For any given field, this is done by assigning the initial value to
+    form.<field-name>.data.
+    """
+    form = EditProfileForm()
+    if form.validate():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        # get_object is used to map a Python object to a db table
+        db.session.add(current_user._get_current_object())
+        db.session.commit()
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+# Profile edit route for admins
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated.')
+        return redirect(usr_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
