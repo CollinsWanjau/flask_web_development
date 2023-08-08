6c39380720aa70fcb0fdcd7b3cf82e31d6ef10b4
colloso999 <tripperskripper@gmail.com>
2023-08-08 21:27:48 +0300

    test cases for gravatar

--- flasky/tests/test_user_model.py
+++ flasky/tests/test_user_model.py
@@ -137,3 +137,22 @@ class UserModelTestCase(unittest.TestCase):
         last_seen_before = u.last_seen
         u.ping()
         self.assertTrue(u.last_seen > last_seen_before)
+
+    def test_gravatar(self):
+        u = User(email='john@example.com', password='cat')
+        """By using app.test_request(), you ensure that the code within
+        the test is executed within the context of an active request.Allowing
+        you to access session and other request-related issues"""
+        with self.app.test_request_context('/'):
+            gravatar = u.gravatar()
+            gravatar_256 = u.gravatar(size=256)
+            gravatar_pg = u.gravatar(rating='pg')
+            gravatar_retro = u.gravatar(default='retro')
+        with self.app.test_request_context('/', base_url='https://example.com'):
+            gravatar_ssl = u.gravatar()
+        self.assertFalse('http://www.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6'in gravatar)
+        self.assertTrue('s=256' in gravatar_256)
+        self.assertTrue('r=pg' in gravatar_pg)
+        self.assertTrue('https://secure.gravatar.com/avatar/' +
+                        'd4c74594d841139328695756648b6bd6' in gravatar_ssl)
