9ecf0b036c55c20cfb9965ccdeb245dce9a14dca
colloso999 <tripperskripper@gmail.com>
2023-08-22 10:50:48 +0300

    Permanent links to posts

--- flasky/app/main/views.py
+++ flasky/app/main/views.py
@@ -175,3 +175,9 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+# Permanent links to post
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
